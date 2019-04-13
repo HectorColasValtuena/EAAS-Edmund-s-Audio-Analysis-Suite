95081f84ffe6e936627c9bf0eda25d9c52483229
hectorcolasvaltuena <mephistoII666@gmail.com>
2019-04-13 23:17:31 +0200

    Moved Persistors into their own folder

--- Persistor/EAAS-Persistor-PropertyKeyframesToObjectArray.jsx
+++ Persistor/EAAS-Persistor-PropertyKeyframesToObjectArray.jsx
@@ -2,7 +2,7 @@
 //Property source to keyframe values array
     //takes a reference to a layer property with keyframes
     //returns an array of {value, time} objects representing those keyframes
-    function propertyToArray (srcProp) {
+    function propertyKeyframesToObjectArray (srcProp) {
         var tarArr = new Array();
         var numKeys = srcProp.numKeys;
         for (var i = 1; i <= numKeys; i++) {
