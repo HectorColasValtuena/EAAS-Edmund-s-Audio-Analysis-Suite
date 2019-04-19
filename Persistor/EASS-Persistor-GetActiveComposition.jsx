2eae6af5675e09a78b271414952464c2b2d15869
hectorcolasvaltuena <mephistoII666@gmail.com>
2019-04-19 04:23:48 +0200

    Added persistor getActiveComposition method

---
+++ Persistor/EASS-Persistor-GetActiveComposition.jsx
@@ -0,0 +1,20 @@
+/**********************************************************************************************\
+*                             Edmund's Audio Synchronization Suite
+*                          Programmed by Héctor Colás Valtueña @2019
+\**********************************************************************************************/
+
+//@include "../EASS-Namespace.jsx"
+
+//@include "./EASS-Constants.jsx"
+
+/** @function getActiveComposition
+ *  Fetches and returns a reference to currently active composition. Returns null if no active item or active item is not a Composition.
+ *
+ *  @returns {Composition} - Reference to active Composition object or null.
+ */
+EASS.Persistor.getActiveComposition = function () {
+	return (app.project.activeItem !== null && app.project.activeItem.typeName == this.Constants.itemTypeNames.composition)
+		? app.project.activeItem
+		: null
+	;
+}
