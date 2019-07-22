6e8036849153c43fc037724cdb1614034fe2ebe7
hectorcolasvaltuena <mephistoII666@gmail.com>
2019-07-22 19:05:19 +0200

    Renamed Persistor WriteMarkers script

--- Persistor/EASS-Persistor-WriteMarkers.jsx
+++ Persistor/EASS-Persistor-WriteMarkers.jsx
@@ -35,6 +35,19 @@ EASS.Persistor.writeObjectArrayToCompositionMarkers = function (objectArray, tar
 	this.writeMarkerArrayToCompositionMarkers(markerArray, targetComp);
 }
 
+/** @function writeObjectArrayToCompositionMarkers
+ *  Writes an array of keyframe objects as markers to a composition maker row.
+ *	If no composition assigned, markers will be written to active composition if found.
+ *
+ *  @param {Array.<{value, time, ...}>} objectArray - Array of keyframe objects to write. "comment" and "duration" attributes will be used to set some marker attributes.
+ *  @param {CompItem} [targetComp] - [OPTIONAL] Composition to write markers to. Will try to find active composition if omitted.
+ *  @param {Object} [maskObj] - [OPTIONAL] Mask indicating what object properties to persist into a marker value.
+ */
+EASS.Persistor.writeObjectArrayToPropertyMarkers = function (objectArray, targetProp, maskObj) {
+	var markerArray = this.keyframeObjectArrayToMarkerValueArray(objectArray, maskObj);
+	this.writeMarkerArrayToPropertyMarkers(markerArray, targetProp);
+}
+
 /** @function writeMarkerArrayToLayerMarkers
  *  Writes an array of MarkerValue objects to a layer's markers property.
  *
@@ -42,7 +55,7 @@ EASS.Persistor.writeObjectArrayToCompositionMarkers = function (objectArray, tar
  *  @param {AVLayer} targetLayer - Target layer.
  */
 EASS.Persistor.writeMarkerArrayToLayerMarkers = function (markerArray, targetLayer) {
-	this.writeMarkerArrayToProperty(markerArray, targetLayer.property("Marker"));
+	this.writeMarkerArrayToPropertyMarkers(markerArray, targetLayer.property("Marker"));
 }
 
 /** @function writeMarkerArrayToCompositionMarkers
@@ -57,17 +70,17 @@ EASS.Persistor.writeMarkerArrayToCompositionMarkers = function (markerArray, tar
 		targetComp = this.getActiveComposition();
 	}
 
-	this.writeMarkerArrayToProperty(markerArray, targetComp.property("Marker"));
+	this.writeMarkerArrayToPropertyMarkers(markerArray, targetComp.property("Marker"));
 }
 
-/** @function writeMarkerArrayToProperty
+/** @function writeMarkerArrayToPropertyMarkers
  *  Writes an array of MarkerValue objects to a target property.
  *	Property should correspond to a marker property.
  *
  *  @param {Array.<MarkerValue>} markerArray - Array of MarkerValue objects to write.
  *  @param {AVLayer} targetLayer - Target layer.
  */
- EASS.Persistor.writeMarkerArrayToProperty = function (markerArray, targetProp) {
+ EASS.Persistor.writeMarkerArrayToPropertyMarkers = function (markerArray, targetProp) {
  	var timeArray = new Array();
  	for (var i = 0, l = markerArray.length; i < l; i++) {
  		timeArray.push(markerArray[i].getParameters().time);
