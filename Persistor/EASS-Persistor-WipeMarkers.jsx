351e6dc23084f3065d8d56eb2852e56b88547511
hectorcolasvaltuena <mephistoII666@gmail.com>
2019-07-24 17:10:24 +0200

    Fixed WipeMarkers comments and spacing

--- Persistor/EASS-Persistor-WipeMarkers.jsx
+++ Persistor/EASS-Persistor-WipeMarkers.jsx
@@ -5,14 +5,14 @@
 
 //@include "../EASS-Namespace.jsx"
 
-//@include "../EASS-WipePropertyKeyframes.jsx"
+//@include "./EASS-Persistor-WipePropertyKeyframes.jsx"
 
-/** @function wipePropertyMarkers
- *  Deletes every keyframe from received AE Property.
+/** @function wipeLayerMarkers
+ *  Deletes every marker from target layer
  *
  *  @param {Property} tarProp - Reference to an AE Property. Every keyframe in the property will be removed
  *  @returns {Property} - Returns again a reference to target property so as to allow chained invocation.
  */
- EASS.Persistor.wipeLayerMarkers = function (targetLayer) {
+EASS.Persistor.wipeLayerMarkers = function (targetLayer) {
  	this.wipePropertyKeyframes(targetLayer.property("Marker"));
- }
+}
